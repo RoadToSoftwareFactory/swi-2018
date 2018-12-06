fcf8920544dabe8b8d089c6e5c1e976acba5020b
Diana Brnovik <dia.brnovikova@gmail.com>
2018-12-06 09:58:10 +0100

    last commit

--- double_hash.rb
+++ double_hash.rb
@@ -1,5 +1,4 @@
-h = {:bacon => 200, :coconut => 300}
-h.each { |k,v| h[k] = v*2 }
-
 # result is {:bacon=>400, :coconut=>600}
 
+h = {:bacon => 200, :coconut => 300}
+h.transform_values! { |v| v * 2 }
